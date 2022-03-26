161e20b3ab6578a1e03deb76c6e7acdf88d6eee9
FAMMMI <naimfami@gmail.com>
2022-03-26 18:12:37 +0600

    ?

--- src/components/QuestionAnswer/QuestionAnswer.js
+++ src/components/QuestionAnswer/QuestionAnswer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './QuestionAnswer.css'
 
 const QuestionAnswer = () => {
     return (
-        <div>
+        <div className='Question-container'>
             <div>
                 <h1>1. How React Works ?</h1>
                 <p><strong>Answer: </strong>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It designs views for each state in the application, updates, and renders just the right component when the data changes. Hence the declarative view makes that code more predictable and easier to debug. A React application is made of multiple components and each of them is responsible for rendering a small, reusable piece of HTML. Components can be nested within other components and doing that, allows complex applications to be built out of simple building blocks. React uses virtual dom in which it reserves the recent changes and it compares it with the react-dom, then discovers the changes between these two and updates the actual dom.</p>
